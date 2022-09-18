d5bf6fb18e0f2467ba27cf8ec05533f72a7db850
brandontaylor156 <brandon.david4@outlook.com>
2022-09-17 17:55:59 -0700

    board style update

--- src/components/applets/tetris/hooks/useGameStats.jsx
+++ src/components/applets/tetris/hooks/useGameStats.jsx
@@ -10,8 +10,13 @@ const buildGameStats = () => ({
 export const useGameStats = () => {
   const [gameStats, setGameStats] = useState(buildGameStats());
 
+  // lines = new amount of lines cleared (1-4)
   const addLinesCleared = useCallback((lines) => {
+
+    // setGameStats (and ANY setter for useState hook) can be passed an optional function which 
+    // receives the previous value of state and return an updated value
     setGameStats((previous) => {
+
       const points = previous.points + lines * 100;
       const { linesPerLevel } = previous;
       const newLinesCompleted = previous.linesCompleted + lines;
@@ -19,14 +24,18 @@ export const useGameStats = () => {
         newLinesCompleted >= linesPerLevel
           ? previous.level + 1
           : previous.level;
+
+      // 10 lines per level
       const linesCompleted = newLinesCompleted % linesPerLevel;
 
+      // This return is the new state of gameStats
       return {
         level,
         linesCompleted,
         linesPerLevel,
         points
       };
+      // still not sure about this second empty array argument
     }, []);
   }, []);
 
