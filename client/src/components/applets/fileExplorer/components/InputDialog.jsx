34fe4a0ecc5ce0ae9e5649d290336bd90f44c1d9
cmderobertis <cmderobertis@gmail.com>
2022-09-21 22:03:57 -0700

    folder/file create functionality complete, UI

--- client/src/components/applets/fileExplorer/components/InputDialog.jsx
+++ client/src/components/applets/fileExplorer/components/InputDialog.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react"
 import DialogBase from "./DialogBase"
 
-const InputDialog = ({ title, placeholder }) => {
+const InputDialog = ({
+  title,
+  placeholder,
+  description,
+  setDir,
+  setNewFolder,
+  setNewFile,
+}) => {
   const [data, setData] = useState({ text: "" })
 
   return (
-    <DialogBase
-      title={title}
-      placeholder={placeholder}
-      data={data}
-      setData={setData}
-      options={{ cancelLabel: "Cancel", submitLabel: "Save" }}
-    />
+    <div className="position-static">
+      <DialogBase
+        title={title}
+        placeholder={placeholder}
+        description={description}
+        data={data}
+        setData={setData}
+        setDir={setDir}
+        setNewFolder={setNewFolder}
+        setNewFile={setNewFile}
+        options={{ cancelLabel: "Cancel", submitLabel: "Save" }}
+      />
+    </div>
   )
 }
 
