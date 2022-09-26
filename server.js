9b6364824638b46ca1542f1ce8eb7f46df49c720
brandontaylor156 <brandon.david4@outlook.com>
2022-09-26 13:47:07 -0700

    reverted

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(cors());
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:5173",
-    origin: "http://34.233.133.111",
+    origin: "http://localhost:5173",
+    // origin: "http://34.233.133.111",
     methods: ["GET", "POST"]
   }
 })
