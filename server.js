2bf3d74a4f8d8107d6760cd27ed60e5034ca3103
brandontaylor156 <brandon.david4@outlook.com>
2022-09-26 18:24:50 -0700

    server.js edit

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 // server.js
 const express = require("express")
 const app = express()
-const http = require("http")
-
-const {Server} = require('socket.io')
 
 const cors = require('cors');
 app.use(cors());
 
-const server = http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    // origin: "http://localhost:5173",
-    origin: "http://3.85.108.226",
-    methods: ["GET", "POST"]
-  }
-})
-
-server.listen(8000, () =>
+const server = app.listen(8000, () =>
   console.log("The server is all fired up on port 8000")
 )
+
+// To initialize the socket, we need to
+// invoke the socket.io library
+// and pass it our Express server
+const io = require("socket.io")(server, { cors: true })
 
 io.on("connection", (socket) => {
   console.log("New connection -->", socket.id)
